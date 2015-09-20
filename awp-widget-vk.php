df003dec3f560f273b246d44eb7ae115911cf653
andriih <andrii.hnatyshyn@gmail.com>
2015-09-20 23:19:57 +0300

    awp_gets function

--- awp-widget-vk.php
+++ awp-widget-vk.php
@@ -17,6 +17,10 @@ function awp_vk ()
 
 class AWP_Vk extends WP_Widget
 {
+	public $title , $count ;
+
+
+
 	function __construct ()
 	{
 		$args = array(
@@ -27,5 +31,29 @@ class AWP_Vk extends WP_Widget
 		return parent::__construct('awp_vk','AWP VK Widget',$args);
 	}
 
-	
+	function widget ($args , $instance)
+	{
+		extract($args);
+		extract($instance);
+		$title = '1234354';
+		$count = 3;
+
+		$this->title = $title;
+		$this->count = $count;
+
+		$data = $this->awp_get_posts_vk();
+		var_dump($data);
+	}
+
+	private function awp_get_posts_vk()
+	{
+		if(is_numeric($this->title) ){
+			$id = "owner_id={$this->title}";
+		}else{
+			$id = "domain={$this->title}";
+		}
+
+		if(!(int)$this->count ) $this->count = 3;
+		return $url = "http://api.vk.com/method/wall.get?{$id}&filter=owner$count={$this->count}";
+	}
 }
